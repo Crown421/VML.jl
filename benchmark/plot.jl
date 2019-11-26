fcd0281b2a2759ac0554372215bec429ea51e812
Steffen <steffen@robots.ox.ac.uk>
2019-11-26 15:37:24 +0000

    Checkpoint for benchmarking

--- benchmark/plot.jl
+++ benchmark/plot.jl
@@ -1,23 +1,38 @@
+using Plots
+using Statistics
+pyplot()
 
-vals = reduce(hcat, collect(values(resSD64)))
-labels = collect(keys(resSD64))
+function plotextendedbm(resSD)
 
-p = plot(layout = (2,2))
-idx = floor.(Int, collect(range(0,31, length = 5)))
+    nArray = [i*10^j for i=1:2:9, j = 3:5][:]
 
+    vals = reduce(hcat, collect(values(resSD)))
+    labels = collect(keys(resSD))
 
-# order by size
-for i in 1:length(idx)-1
-    specidx = idx[i]+1:idx[i+1]
-    specvals = vals[:, specidx]
-    order = sortperm(mean(specvals, dims = 1)[:], rev = true)
-    plot!(p, nArray, specvals[:, order], labels = permutedims(string.(labels[specidx])), 
-    subplot = i, 
-    xscale = :log10, yscale = :log10,
-    linewidth = 2,
-    yticks = (1:3:12, string.(collect(1:3:12))),
-    # xticks = (1:15, nArray)
-    )
+    p = plot(layout = (2,2), size =(1100, 800))
+    idx = floor.(Int, collect(range(0,31, length = 5)))
+
+    # order by size
+    for i in 1:length(idx)-1
+        specidx = idx[i]+1:idx[i+1]
+        specvals = vals[:, specidx]
+        order = sortperm(mean(specvals, dims = 1)[:], rev = true)
+        plot!(p, nArray, specvals[:, order], labels = permutedims(string.(labels[specidx])), 
+        subplot = i,
+        xlabel = "input array size", ylabel = "performance ratio",
+        xscale = :log10, yscale = :log10,
+        linewidth = 2,
+        yticks = (1:3:12, string.(collect(1:3:12))),
+        # xticks = (1:15, nArray)
+        )
+    end
+
+    return p
 end
 
-p
+
+p = plotextendedbm(resSD64)
+savefig(p, "performance_ex64.png")
+
+p = plotextendedbm(resSD32)
+savefig(p, "performance_ex32.png")
